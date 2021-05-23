ce87bb35ab78a47282fbed40a695c76d03e63d4a
hjo237 <78175046+hjo237@users.noreply.github.com>
2021-05-23 17:53:32 -0400

    app.py done

--- app.py
+++ app.py
@@ -16,21 +16,13 @@ app.config['UPLOAD_PATH'] = 'uploads'
 
 # load credentials and configuration options from .env file
 # if you do not yet have a file named .env, make one based on the template in env.example
-import credentials
-config = credentials.get()
 
 # turn on debugging if in development mode
-if config['FLASK_ENV'] == 'development':
+'''
+if app.config['FLASK_ENV'] == 'development':
     # turn on debugging, if in development
     app.debug = True # debug mnode
-
-# make one persistent connection to the database
-connection = pymongo.MongoClient(config['MONGO_HOST'], 27017, 
-                                username=config['MONGO_USER'],
-                                password=config['MONGO_PASSWORD'],
-                                authSource=config['MONGO_DBNAME'])
-db = connection[config['MONGO_DBNAME']] # store a reference to the database
-
+'''
 # set up the routes
 
 @app.route('/')
